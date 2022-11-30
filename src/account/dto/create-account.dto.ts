99f14a0d3d770a8412ca6c390114b0892973285f
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2022-11-29 22:02:59 -0300

    ADD:swagger for account

--- src/account/dto/create-account.dto.ts
+++ src/account/dto/create-account.dto.ts
@@ -1,13 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumber } from 'class-validator';
 import { User } from 'src/auth/entities/user.entity';
 
 export class CreateAccountDto {
+
+  @ApiProperty({
+    example: '3450204958690'
+  })
   @IsString()
   numAccount: string;
 
+  @ApiProperty({
+    example: '123456'
+  })
   @IsNumber()
   amount: number;
 
+  @ApiProperty({
+    example: 'UUID: 16763be4-6022-406e-a950-fcd5018633ca'
+  })
   @IsString()
   userId: User;
 }
