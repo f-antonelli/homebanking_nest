7ccf9e2695aef3cf20a16a21c5ca3e488ade3352
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2022-12-02 20:05:53 -0300

    ADD: card's services

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,26 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { isUUID } from 'class-validator';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Repository } from 'typeorm';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { Card } from './entities/card.entity';
 
 @Injectable()
 export class CardsService {
-  create(createCardDto: CreateCardDto) {
-    return 'This action adds a new card';
+  constructor(
+    @InjectRepository(Card)
+    private readonly cardRepository: Repository<Card>,
+  ){}
+
+  async create(createCardDto: CreateCardDto) {
+    try {
+      const card = this.cardRepository.create({
+        ...createCardDto,
+      });
+      await this.cardRepository.save(card);
+      return card;
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all cards`;
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    
+    return  this.cardRepository.find({
+      take: limit,
+      skip: offset,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} card`;
+  async findOne(uuid: string) {
+    let card: Card;
+
+    if(isUUID(uuid)){
+      card = await this.cardRepository.findOneBy({
+        id: uuid,
+      });
+      return card;
+    } else {
+      const queryBuilder = this.cardRepository.createQueryBuilder('card');
+      card = await queryBuilder.where('card.id = :id', { uuid }).getOne();
+    }
+    if(!card) throw new NotFoundException(`Card with ${uuid} not found`);
   }
 
-  update(id: number, updateCardDto: UpdateCardDto) {
-    return `This action updates a #${id} card`;
+  async update(id: string, updateCardDto: UpdateCardDto) {
+    const card = await this.cardRepository.preload({
+      id,
+      ...updateCardDto,
+    });
+
+    if(!card){
+      throw new NotFoundException(`Card with id ${id} not found`);
+    }
+
+    try {
+      await this.cardRepository.save(card);
+      return card;
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} card`;
+  async remove(id: string) {
+    const card = await this.cardRepository.findOneBy({ id });
+    await this.cardRepository.remove(card);
+  }
+
+  private handleDBErrors(error: any): never {
+    console.log(error);
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+    console.log(error);
+    throw new InternalServerErrorException('Please check server logs');
   }
 }
