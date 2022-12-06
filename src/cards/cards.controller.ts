bf1e41571a7b8d81ee9a184c4086f0196ec3a1f3
gonzacabrera2506 <gonzacabrera2506@gmail.com>
2022-12-05 23:23:37 -0300

    ADD: Swagger for Card's endpoints

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -9,43 +9,90 @@ import {
   Query,
   ParseUUIDPipe,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiForbiddenResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags, ApiUnprocessableEntityResponse } from '@nestjs/swagger';
 import { Auth } from 'src/auth/decorators';
 import { ValidRoles } from 'src/auth/interfaces/valid-roles.interface';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { CardsService } from './cards.service';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
+import { Card } from './entities/card.entity';
 
 @ApiTags('Cards')
 @Controller('cards')
 export class CardsController {
   constructor(private readonly cardsService: CardsService) {}
 
+  @ApiCreatedResponse({
+    type: Card,
+    description: 'Card created succesfully',
+  })
+  @ApiUnprocessableEntityResponse({
+    description: 'Bad request',
+  })
   @Post()
   @Auth(ValidRoles.user)
   create(@Body() createCardDto: CreateCardDto) {
     return this.cardsService.create(createCardDto);
   }
 
+  @ApiOkResponse({
+    description: 'The resources were returned succesfully',
+  })
+  @ApiForbiddenResponse({
+    description: 'Unauthorized request',
+  })
+  @ApiNotFoundResponse({
+    description: 'Resource not found',
+  })
   @Get()
   @Auth(ValidRoles.admin)
   findAll(@Query() paginationDto: PaginationDto) {
     return this.cardsService.findAll(paginationDto);
   }
 
+  @ApiOkResponse({
+    description: 'The resources were returned succesfully',
+  })
+  @ApiForbiddenResponse({
+    description: 'Unauthorized request',
+  })
+  @ApiNotFoundResponse({
+    description: 'Resource not found',
+  })
   @Get(':uuid')
   @Auth(ValidRoles.user)
   findOne(@Param('uuid') uuid: string) {
     return this.cardsService.findOne(uuid);
   }
 
+  @ApiOkResponse({
+    description: 'The resources was updated succesfully',
+  })
+  @ApiNotFoundResponse({
+    description: 'Resource not found',
+  })
+  @ApiForbiddenResponse({
+    description: 'Unauthorized request',
+  })
+  @ApiUnprocessableEntityResponse({
+    description: 'Bad request',
+  })
   @Patch(':id')
   @Auth(ValidRoles.user)
   update(@Param('id') id: string, @Body() updateCardDto: UpdateCardDto) {
     return this.cardsService.update(id, updateCardDto);
   }
 
+  @ApiOkResponse({
+    description: 'The resource was deleted succesfully',
+  })
+  @ApiForbiddenResponse({
+    description: 'Unauthorized request',
+  })
+  @ApiNotFoundResponse({
+    description: 'Resource not found',
+  })
   @Delete(':id')
   @Auth(ValidRoles.user)
   remove(@Param('id', ParseUUIDPipe) id: string) {
